db3e005d68206e5e51c9f0dcf27285558dcd338d
aasthasmile <aasthasmile.jain@gmail.com>
2017-02-14 14:44:41 -0800

    Completed 32 to 40 Quoestions from geeks4Geeks.
    More Practice Problems to be added soon.

--- src/linkedList/LinkedListNode.java
+++ src/linkedList/LinkedListNode.java
@@ -3,9 +3,11 @@
  */
 package linkedList;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Stack;
+
 
 /**
  * @author Aastha Jain
@@ -178,6 +180,32 @@ public class LinkedListNode<T> {
 		return lastnode;
 	}
 
+	public static LinkedListNode reverseIteratively(LinkedListNode head) {
+		if (head == null || head.next == null)
+			return null;
+		LinkedListNode curr = head;
+		LinkedListNode prev = null;
+		LinkedListNode next = null;
+
+		// Iterative code for reversing the linked list
+		// Steps can be explained as :
+		// 1. Take three Nodes previous ,current ,next
+		// 2. Initially prev=null eg null->2->3->4->5
+		// 3. Assign current to Head node i.e. 2->
+		// 4. Assign next to current to next .
+		// 5.In a loop, change next to prev, prev to current and current to
+		// next.(Iteratively).
+		while (curr != null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+
+		}
+		head = prev;
+		return head;
+	}
+
 	/*
 	 * 25. Remove duplicates from a sorted linked list
 	 */
@@ -295,7 +323,11 @@ public class LinkedListNode<T> {
 	}
 
 	/**
-	 * Deleting alternate Nodes in a Linked List
+	 * 31. Deleting alternate Nodes in a Linked List using Recursion . Time
+	 * Complexity : O(n)
+	 * 
+	 * Method : Pick the link of the second node pointer and place that in the
+	 * link of the first node pointer.Delete the Second node.
 	 * 
 	 * @param head
 	 */
@@ -303,140 +335,363 @@ public class LinkedListNode<T> {
 	public static void deleteAlternateNode(LinkedListNode head) {
 		if (head == null)
 			return;
-		LinkedListNode node=head.next;
+		LinkedListNode node = head.next;
 		head.next = node.next;
-		node=null;
+		node = null;
 		deleteAlternateNode(head.next);
-		
+
 	}
 
-	public static void main(String[] args) {
+	/**
+	 * 32. Alternating split of a given Singly Linked List
+	 * 
+	 * @param args
+	 */
+	public static void AlternatingSplit(LinkedListNode head) {
+		if (head == null)
+			return;
 
-		LinkedListNode head = null;
+		LinkedListNode a = null;
+		LinkedListNode b = null;
 
-		// Creating A Linked List
-		for (int i = 10; i < 16; i++) {
-			if (head == null)
-				head = new LinkedListNode(i / 2);
+		while (head.next != null) {
+			if (a == null)
+				a = new LinkedListNode(head.data);
 			else
-				head.insertAtEnd(i);
+				a.insertAtEnd(head.data);
+
+			head = head.next;
+
+			if (b == null)
+				b = new LinkedListNode(head.data);
+			else
+				b.insertAtEnd(head.data);
+
+			if (head.next != null)
+				head = head.next;
 		}
 
-		System.out.println("Intial Linked List :");
-		LinkedListNode.printLinkedList(head);
+		System.out.print("List 1 : ");
+		LinkedListNode.printLinkedList(a);
+		System.out.print("List 2 : ");
+		LinkedListNode.printLinkedList(b);
+	}
 
-		/*
-		 * 22.Linked list is Palindrome or Not
-		 */
-		char str[] = { 'a', 'b', 'c', 'd', 'c', 'b', 'a' };
-		LinkedListNode head22 = null;
-		for (int i = 0; i < str.length; i++) {
-			if (i == 0)
-				head22 = new LinkedListNode(str[i]);
-			else
-				head22.insertAtEnd(str[i]);
-		}
+	/**
+	 * Identical Linked Lists using Recursion :Two Linked Lists are identical
+	 * when they have same data and arrangement of data is also same. For
+	 * example Linked lists a (1->2->3) and b(1->2->3) are identical.
+	 * 
+	 * @param args
+	 */
 
-		System.out.println("22. Linked list is Palindrome or Not :");
-		LinkedListNode.printLinkedList(head22);
-		System.out.println("Palindrome is: " + LinkedListNode.palindrome(head22));
+	public static boolean areIdentical(LinkedListNode head1, LinkedListNode head2) {
+		if (head1 == null && head2 == null)
+			return true;
 
-		/*
-		 * 23. Intersection Linked-List
-		 */
-		LinkedListNode head2 = null;
-		for (int i = 12; i < 16; i++) {
-			if (head2 == null)
-				head2 = new LinkedListNode(i % 4);
-			else
-				head2.insertAtEnd(i);
-		}
-
-		System.out.println("\n23.  2 linked list are:- ");
-		LinkedListNode.printLinkedList(head);
-		LinkedListNode.printLinkedList(head2);
-		System.out.println("The point of intersection is :");
-		LinkedListNode.intersection(head, head2);
-
-		/*
-		 * 24. Reversing Linked-List
-		 */
-		System.out.println("\n24. Linked list is:- ");
-		LinkedListNode.printLinkedList(head);
-		LinkedListNode.reverseRecursively(head);
-		System.out.println("\nAfter reversing Linked list is:- ");
-		LinkedListNode.printLinkedList(head);
-
-		/* 25. Remove Duplicates */
-		LinkedListNode head25 = null;
-		for (int i = 12; i < 20; i++) {
-			if (i == 12)
-				head25 = new LinkedListNode(i);
-			else if (i > 12 & i < 16)
-				head25.insertAtEnd(18);
-			else
-				head25.insertAtEnd(i);
-		}
-		System.out.println("\n25. Linked list is:- ");
-		LinkedListNode.printLinkedList(head25);
-		LinkedListNode.removeDuplicates(head25);
-		LinkedListNode.printLinkedList(head25);
-
-		/* 26. Remove Duplicates Unsorted List */
-		System.out.println("\n26. Linked list is:- ");
-		LinkedListNode head26 = null;
-		for (int i = 12; i < 20; i++) {
-			if (head26 == null)
-				head26 = new LinkedListNode(i + 3);
-			else
-				head26.insertAtEnd(i);
+		if (head1 != null && head2 != null) {
+			if (head1.data != head2.data)
+				return false;
 
 		}
-		LinkedListNode.printLinkedList(head26);
-		LinkedListNode.removeDuplicatesUnsorted(head26);
-		LinkedListNode.printLinkedList(head26);
+		return areIdentical(head1.next, head2.next);
+	}
 
-		/* 27. Pairwise Swap */
-		System.out.println("\n27. Linked list is:- ");
-		LinkedListNode.printLinkedList(head);
-		// LinkedListNode.pairwiseSwap(head);
-		System.out.println("After Paiwise Swap:- ");
-		LinkedListNode.printLinkedList(head);
+	/**
+	 * Merge Sort for Linked Lists Let head be the first node of the linked list
+	 * to be sorted and headRef be the pointer to head. Note that we need a
+	 * reference to head in MergeSort() as the below implementation changes next
+	 * links to sort the linked lists (not data at the nodes), so head node has
+	 * to be changed if the data at original head is not the smallest value in
+	 * linked list
+	 */
 
-		/*
-		 * Recheck solution --- 29. Move last element to front of a given Linked
-		 * List System.out.println("\n29. Linked list is:- "); LinkedListNode
-		 * head29 = null; for (int i = 12; i < 17; i++) { if (head29 == null)
-		 * head29 = new LinkedListNode(i); else head29.insertAtEnd(i);
-		 * 
-		 * }
-		 * 
-		 * LinkedListNode.printLinkedList(head29);
-		 * LinkedListNode.moveLasttoFirst(head29); System.out.
-		 * println("After moving last element to the front of a list:- ");
-		 * LinkedListNode.printLinkedList(head29);
-		 */
+	public static HashMap FrontBackSplit(LinkedListNode head) {
+		if (head == null)
+			return null;
 
-		/*
-		 * 30. Intersection of two Linked list
-		 */
-		System.out.println("\n30. 2 Linked list is:- ");
-		LinkedListNode.printLinkedList(head);
-		LinkedListNode.printLinkedList(head2);
-		LinkedListNode head3 = LinkedListNode.intersectionSorted(head, head2);
-		System.out.println("\nIntersection of Sorted Linked list is:- ");
-		LinkedListNode.printLinkedList(head3);
+		LinkedListNode list1 = null;
+		LinkedListNode list2 = null;
+		int size = head.sizeOfLinkedList(head);
+		int count = 1;
+		while (head.next != null) {
 
-		/*
-		 * 31. Delete alternate nodes in a linked list
-		 */
-		System.out.println("-----------31---------------");
-		System.out.println("Delete alternate nodes in a Linked list is:- ");
-		LinkedListNode.printLinkedList(head);
-		LinkedListNode.deleteAlternateNode(head);
-		LinkedListNode.printLinkedList(head);
-		System.out.println("--------------------------");
+			if (count <= size / 2) {
+				if (list1 == null)
+					list1 = new LinkedListNode(head.data);
+				else
+					list1.insertAtEnd(head.data);
 
+				head = head.next;
+				count++;
+			} else {
+				if (list2 == null)
+					list2 = new LinkedListNode(head.data);
+				else
+					list2.insertAtEnd(head.data);
+
+				if (head.next != null)
+					head = head.next;
+			}
+
+		}
+		if (size % 2 != 0)
+			list2.insertAtEnd(head.data);
+
+		HashMap<Integer, LinkedListNode> map = new HashMap<>();
+		map.put(1, list1);
+		map.put(2, list2);
+		return map;
+
+	}
+
+	public static void MergeSort(LinkedListNode head) {
+
+		if (head == null || head.next == null)
+			return;
+
+		HashMap<Integer, LinkedListNode> map = new HashMap<>();
+		map = FrontBackSplit(head);
+		LinkedListNode list1 = map.get(1);
+		LinkedListNode list2 = map.get(2);
+
+		System.out.print("List1 ");
+		LinkedListNode.printLinkedList(list1);
+		System.out.print("List2 ");
+		LinkedListNode.printLinkedList(list2);
+
+		while (list1 != null)
+			MergeSort(list1);
+		while (list2 != null)
+			MergeSort(list2);
+
+		head = SortedMerge(list1, list2);
+		LinkedListNode.printLinkedList(head);
+	}
+
+	public static LinkedListNode SortedMerge(LinkedListNode head1, LinkedListNode head2) {
+		if (head1 == null)
+			return head2;
+		if (head2 == null)
+			return head1;
+		LinkedListNode head3 = null;
+		while (head1 != null && head2 != null) {
+			if ((int) head1.data <= (int) head2.data) {
+				head3 = head1;
+				head3 = SortedMerge(head1.next, head2);
+			} else {
+				head3 = head2;
+				head3 = SortedMerge(head1, head2.next);
+			}
+		}
+		return head3;
+
+	}
+
+	/**
+	 * Reverse a Linked List in groups of given size. Given a linked list, write
+	 * a function to reverse every k nodes (where k is an input to the
+	 * function).
+	 */
+	public static LinkedListNode reverseKnodes(LinkedListNode head, int k) {
+		if (head == null || head.next == null)
+			return null;
+		LinkedListNode curr = head;
+		LinkedListNode prev = null;
+		LinkedListNode next = null;
+
+		int count = 1;
+
+		// Iterative code for reversing the linked list
+		// Steps can be explained as :
+		// 1. Take three Nodes previous ,current ,next
+		// 2. Initially prev=null eg null->2->3->4->5
+		// 3. Assign current to Head node i.e. 2->
+		// 4. Assign next to current to next .
+		// 5.In a loop, change next to prev, prev to current and current to
+		// next.(Iteratively).
+		while (head != null && count <= k) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+			count++;
+		}
+
+		if (next != null)
+			head.next = reverseKnodes(next, k);
+
+		/* prev becomes the new head of the list */
+		return prev;
+
+	}
+
+	/**
+	 * 37. Delete nodes which have a greater value on right side The list
+	 * 12->15->10->11->5->6->2->3->NULL should be changed to 15->11->6->3->NULL.
+	 * Note that 12, 10, 5 and 2 have been deleted because there is a greater
+	 * value on the right side.
+	 */
+	public static LinkedListNode GreaterRightSide(LinkedListNode head) {
+		if (head == null || head.next == null)
+			return null;
+
+		// Method 2 (Use Reverse)
+		// 1. Reverse the list.
+		// 2. Traverse the reversed list. Keep max till now. If next node < max,
+		// then delete the next
+		// node, otherwise max = next node.
+		// 3. Reverse the list again to retain the original order. Time
+		// Complexity: O(n)
+
+		LinkedListNode reversed = reverseIteratively(head);
+
+		LinkedListNode curr = reversed;
+		LinkedListNode maxNode = reversed;
+
+		while (curr != null && curr.next != null) {
+			if ((int) curr.next.data < (int) maxNode.data) {
+				LinkedListNode next_to_next = curr.next.next;
+				curr.next = null;
+				curr.next = next_to_next;
+			} else {
+				maxNode = curr.next;
+				curr = maxNode;
+			}
+		}
+
+		return reverseIteratively(reversed);
+
+	}
+
+	/**
+	 * 38. Segregate even and odd nodes in a Linked List Given a Linked List of
+	 * integers, write a function to modify the linked list such that all even
+	 * numbers appear before all the odd numbers in the modified linked list.
+	 * Also, keep the order of even and odd numbers same.
+	 */
+
+	public static void segregateEvenAndOdd(LinkedListNode head) {
+
+		// The idea is to split the linked list into two: one containing all
+		// even nodes and other containing all odd nodes.
+		// And finally attach the odd node linked list after the even node
+		// linked list.
+
+		if (head == null || head.next == null)
+			return;
+
+		LinkedListNode current = head;
+		LinkedListNode evenplusOdd = null;
+
+		while (current != null) {
+			if ((int) current.data % 2 == 0) {
+				if (evenplusOdd == null)
+					evenplusOdd = new LinkedListNode(current.data);
+				else
+					evenplusOdd.insertAtEnd(current.data);
+			}
+			current = current.next;
+		}
+
+		while (head != null) {
+			if ((int) head.data % 2 != 0) {
+				evenplusOdd.insertAtEnd(head.data);
+			}
+			head = head.next;
+		}
+		LinkedListNode.printLinkedList(evenplusOdd);
+
+	}
+
+	/**
+	 * 39. Detect and Remove Loop in a Linked List.
+	 *  slow and fast pointers meet at some point after Floyd’s Cycle finding algorithm.
+	 *  
+	 *  Algorithms : So if we start moving both pointers again at same speed such that one pointer (say slow) begins from head node
+	 *   of linked list and other pointer (say fast) begins from meeting point. When slow pointer reaches beginning of linked list 
+	 *   (has made m steps). Fast pointer would have made also moved m steps as they are now moving same pace. Since m+k is a multiple
+	 *    of n and fast starts from k, they would meet at the beginning.
+	 */
+
+	public static void detectAndRemoveLoop(LinkedListNode head) {
+		
+		if (head == null || head.next == null)
+			return;
+		
+		//Detecting loop in linked list using Slow and Fast Pointer.
+		LinkedListNode slow=head;
+		LinkedListNode fast=head.next;
+		
+		while(fast!=null && fast.next!=null){
+			if(slow!=fast){
+				slow=slow.next;
+				fast=fast.next.next;
+			}
+			else{
+			   break;
+			}
+		}
+		
+		//if there is a loop in linked list
+		//1.Move slow and fast pointer at same speed.
+		//2.Slow is at head of linked list and fast is at the meeting point.
+		//3. When slow and fast.next meet each other we are at the intersection 
+		//4. We can set fast.next=null to remove the loop
+		if(slow==fast){
+			slow=head;
+			while(slow!=fast.next){
+				slow=slow.next;
+				fast=fast.next;
+			}
+			fast.next=null;
+		}
+		
+		
+		
+	}
+	
+	/**40.
+	 * Adding Two Numbers using Linked List 
+	 * Input:
+		  First List: 5->6->3  // represents number 365
+		  Second List: 8->4->2 //  represents number 248
+		Output
+		  Resultant list: 3->1->6  // represents number 613
+
+	 */
+	
+	public static void AddLinkedListWithCarry(LinkedListNode head1, LinkedListNode head2) {
+		int len1 = LinkedListNode.sizeOfLinkedList(head1);
+		int len2 = LinkedListNode.sizeOfLinkedList(head2);
+
+		if (head1 == null || head2 == null || len1 != len2)
+			return;
+		else {
+			int carry = 0, data = 0;
+			LinkedListNode node3 = null;
+			LinkedListNode head3 = null;
+			while (head1 != null) {
+				
+				data = (((int)head1.data + (int)head2.data) % 10) + carry;
+				carry = ((int)head1.data + (int)head2.data) / 10;
+				
+				if (node3 == null) {
+					node3 = new LinkedListNode(((int)head1.data +(int) head2.data) % 10);
+					head3 = node3;
+				} 
+				else {
+					node3.insertAtEnd(data);
+				}
+				head1 = head1.next;
+				head2 = head2.next;
+				
+			}
+
+			System.out.println("\n\nLinked list after addition:- ");
+			LinkedListNode.printLinkedList(head3);
+
+		}
 	}
 
 }
--- src/linkedList/Solutions.java
+++ src/linkedList/Solutions.java
@@ -4,7 +4,7 @@
 package linkedList;
 
 /**
- * @author Aastha Jain
+ * @author Aastha Jain  29,34
  *
  */
 public class Solutions {
@@ -12,6 +12,8 @@ public class Solutions {
 	/**
 	 * @param args
 	 */
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedListNode head = null;
@@ -30,6 +32,7 @@ public class Solutions {
 		/*
 		 * 22.Linked list is Palindrome or Not
 		 */
+		System.out.println("--------------------------");
 		char str[] = { 'a', 'b', 'c', 'd', 'c', 'b', 'a' };
 		LinkedListNode head22 = null;
 		for (int i = 0; i < str.length; i++) {
@@ -42,12 +45,14 @@ public class Solutions {
 		System.out.println("22. Linked list is Palindrome or Not :");
 		LinkedListNode.printLinkedList(head22);
 		System.out.println("Palindrome is: " + LinkedListNode.palindrome(head22));
+		System.out.println("--------------------------");
 
 		/*
 		 * 23. Intersection Linked-List
 		 */
+		System.out.println("--------------------------");
 		LinkedListNode head2 = null;
-		for (int i = 12; i < 16; i++) {
+		for (int i = 12; i < 17; i++) {
 			if (head2 == null)
 				head2 = new LinkedListNode(i % 4);
 			else
@@ -59,17 +64,21 @@ public class Solutions {
 		LinkedListNode.printLinkedList(head2);
 		System.out.println("The point of intersection is :");
 		LinkedListNode.intersection(head, head2);
+		System.out.println("--------------------------");
 
 		/*
 		 * 24. Reversing Linked-List
 		 */
+		System.out.println("--------------------------");
 		System.out.println("\n24. Linked list is:- ");
 		LinkedListNode.printLinkedList(head);
 		LinkedListNode.reverseRecursively(head);
 		System.out.println("\nAfter reversing Linked list is:- ");
 		LinkedListNode.printLinkedList(head);
+		System.out.println("--------------------------");
 
 		/* 25. Remove Duplicates */
+		System.out.println("--------------------------");
 		LinkedListNode head25 = null;
 		for (int i = 12; i < 20; i++) {
 			if (i == 12)
@@ -83,8 +92,10 @@ public class Solutions {
 		LinkedListNode.printLinkedList(head25);
 		LinkedListNode.removeDuplicates(head25);
 		LinkedListNode.printLinkedList(head25);
-
+		System.out.println("--------------------------");
+		
 		/* 26. Remove Duplicates Unsorted List */
+		System.out.println("--------------------------");
 		System.out.println("\n26. Linked list is:- ");
 		LinkedListNode head26 = null;
 		for (int i = 12; i < 20; i++) {
@@ -97,13 +108,16 @@ public class Solutions {
 		LinkedListNode.printLinkedList(head26);
 		LinkedListNode.removeDuplicatesUnsorted(head26);
 		LinkedListNode.printLinkedList(head26);
+		System.out.println("--------------------------");
 
 		/* 27. Pairwise Swap */
+		System.out.println("-----------27--------------");
 		System.out.println("\n27. Linked list is:- ");
 		LinkedListNode.printLinkedList(head);
 		// LinkedListNode.pairwiseSwap(head);
 		System.out.println("After Paiwise Swap:- ");
 		LinkedListNode.printLinkedList(head);
+		System.out.println("--------------------------");
 
 		/*
 		 * Recheck solution --- 29. Move last element to front of a given Linked
@@ -122,16 +136,16 @@ public class Solutions {
 		/*
 		 * 30. Intersection of two Linked list
 		 */
-		System.out.println("--------------------------");
-		System.out.println("\n30. 2 Linked list is:- ");
+		System.out.println("----------30--------------");
+		System.out.println(" 2 Linked list are  ");
 		LinkedListNode.printLinkedList(head);
 		LinkedListNode.printLinkedList(head2);
 		LinkedListNode head3 = LinkedListNode.intersectionSorted(head, head2);
 		System.out.println("Intersection of Sorted Linked list is:- ");
 		LinkedListNode.printLinkedList(head3);
-		System.out.println("--------------------------");
+		System.out.print("--------------------------");
 
-		/*
+		/**
 		 * 31. Delete alternate nodes in a linked list
 		 */
 		System.out.println("-----------31---------------");
@@ -140,6 +154,114 @@ public class Solutions {
 		LinkedListNode.deleteAlternateNode(head);
 		LinkedListNode.printLinkedList(head);
 		System.out.println("--------------------------");
+		
+		/**
+		 * 32. Alternating split of a given Singly Linked List
+		 */
+		System.out.println("-----------32---------------");
+		System.out.println("Alternating split of a given Singly Linked List ");
+		LinkedListNode.printLinkedList(head2);
+		LinkedListNode.AlternatingSplit(head2);
+		System.out.println("--------------------------");
+		
+		/**
+		 * 33. Identical Linked Lists
+		 */
+		System.out.println("----------33-----------");
+		System.out.println("Identical Singly Linked List ");
+		LinkedListNode.printLinkedList(head);
+		System.out.println("Two list are Identical : "+LinkedListNode.areIdentical(head, head));
+		System.out.println("Two list are Identical : "+LinkedListNode.areIdentical(head, head2));
+		System.out.println("--------------------------");
+		
+		/**
+		 * 34. Merge Sort for Linked Lists
+		 */
+		System.out.println("----------34-----------");
+		System.out.println("Merge Sort for Linked List ");
+		LinkedListNode list=null;
+		for(int i=12;i<22;i++){
+			if(list==null) list=new LinkedListNode(8);
+			else if(i%2==0) list.insertAtEnd(i/2);
+			else if(i%3==0) list.insertAtEnd(i+8);
+			else if(i%4==0) list.insertAtEnd(i+1);
+		}
+		LinkedListNode.printLinkedList(list);
+		//LinkedListNode.MergeSort(list);
+		LinkedListNode.printLinkedList(list);
+		System.out.println("--------------------------");
+		
+		/**35.
+		 * Reverse nodes with a Group of Size k
+		 */
+		System.out.println("----------35-----------");
+		LinkedListNode head35=null;
+		for(int i=12;i<21;i++){
+			if(head35==null) head35=new LinkedListNode(5);
+			else  head35.insertAtEnd(i);
+		}
+		int k=4;
+		System.out.println("Reverse nodes with a Group of Size "+k);
+		LinkedListNode.printLinkedList(head35);
+		LinkedListNode reversedList=LinkedListNode.reverseKnodes(head35, k);
+		LinkedListNode.printLinkedList(reversedList);
+		System.out.println("--------------------------");
+		
+		/**37.
+		 * Delete nodes which have a greater value on right side
+		 */
+		System.out.println("----------37-----------");
+		LinkedListNode head36=null;
+		for(int i=12;i<21;i++){
+			if(head36==null) head36=new LinkedListNode(5);
+			else  head36.insertAtEnd(i%4);
+		}
+		
+		System.out.println("Delete nodes which have a greater value on right side ");
+		LinkedListNode.printLinkedList(head36);
+		LinkedListNode greaterRight=LinkedListNode.GreaterRightSide(head36);
+		LinkedListNode.printLinkedList(greaterRight);
+		System.out.println("--------------------------");
+		
+		/**38.
+		 * Segregate even and odd nodes in a Linked List
+		 */
+		System.out.println("----------38-----------");
+		LinkedListNode.printLinkedList(head2);
+		System.out.println("Segregate even and odd nodes in a Linked List ");
+		LinkedListNode.segregateEvenAndOdd(head2);
+		System.out.println("--------------------------");
+		
+		/**39.
+		 * Detect and Remove Loop in a Linked List.
+		 */
+		System.out.println("----------39-----------");
+		LinkedListNode head39=null;
+		for(int i=12;i<24;i++){
+			if(head39==null) head39=new LinkedListNode(5);
+			else  head39.insertAtEnd(i%4);
+		}
+		head39.next.next.next.next.next = head39.next.next;
+		System.out.println("Detect and Remove Loop in a Linked List. ");
+		LinkedListNode.detectAndRemoveLoop(head39);
+		LinkedListNode.printLinkedList(head39);
+		System.out.println("-------------------------");
+		
+		/**40.
+		 * Adding Two Numbers using Linked List 
+		*/
+		System.out.println("----------40-----------");
+		LinkedListNode head40=null;
+		for(int i=1;i<9;i++){
+			if(head40==null) head40=new LinkedListNode(5);
+			else  head40.insertAtEnd(i%4);
+		}
+		LinkedListNode.printLinkedList(head40);
+		LinkedListNode.printLinkedList(head40);
+		System.out.println("Adding Two Numbers using Linked List ");
+		LinkedListNode.AddLinkedListWithCarry(head40, head40);
+		System.out.println("--------------------------");
+		
 
 	}
 
