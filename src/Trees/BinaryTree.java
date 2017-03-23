19d20da51acf4fb75adc587ad0be0f84c2264b49
aasthasmile <aasthasmile.jain@gmail.com>
2017-03-23 13:06:12 -0700

    Max Element ,Search , Height , Width of a Binary Tree

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -3,46 +3,95 @@ package Trees;
 import java.util.LinkedList;
 import java.util.Queue;
 
-import javax.print.attribute.IntegerSyntax;
-
 public class BinaryTree {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		Treenode root=new Treenode(1);
-		root.left=new Treenode(2);
-		root.right=new Treenode(3);
-		root.left.left=new Treenode(4);
-		root.left.right=new Treenode(5);
-		root.right.left=new Treenode(6);
-		root.right.right=new Treenode(7);
-		
-		System.out.println("\nThe preorder traversal is :");
-		root.preOrderTraversal(root);
-		
-		System.out.println("\nThe Inorder traversal is :");
-		root.InOrderTraversal(root);
-		
-		System.out.println("\nThe postorder traversal is :");
-		root.postOrderTraversal(root);
-		
-		System.out.println("\nThe preorder traversal(iteratively) is :");
-		root.preOrderIterative(root);
-		
-		System.out.println("\nThe Inorder traversal(iteratively) is :");
-		root.InOrderIterative(root);
-		
-		System.out.println("\nThe LevelOrder traversal is :");
-		root.LevelOrderTraversal(root);
-		
-		System.out.println("\nInserting an element in the Binary Tree:");
-		Treenode newroot=root.InsertBT(root, 9);
-		root.InOrderIterative(newroot);
-		
 
-		
+		Treenode<Integer> root = new Treenode<Integer>(1);
+		root.left = new Treenode<Integer>(2);
+		root.right = new Treenode<Integer>(3);
+		root.left.left = new Treenode<Integer>(4);
+		root.left.right = new Treenode<Integer>(5);
+		root.right.left = new Treenode<Integer>(6);
+		root.right.right = new Treenode<Integer>(7);
+
+		System.out.println("\nInserting an element in the Binary Tree:");
+		Tree t = new Tree();
+		Treenode<Integer> rootnode = null;
+		for (int i = 1; i <= 7; i++) {
+			if (rootnode == null)
+				rootnode = new Treenode<Integer>(i);
+			else
+				t.insertInBinaryTree(rootnode, i);
+		}
+
+		t.preOrderTraversal(rootnode);
+
+		System.out.println("\nThe preorder traversal is :");
+		t.preOrderTraversal(root);
+
+		System.out.println("\nThe Inorder traversal is :");
+		t.InOrderTraversal(root);
+
+		System.out.println("\nThe postorder traversal is :");
+		t.postOrderTraversal(root);
+
+		System.out.println("\nThe preorder traversal(iteratively) is :");
+		t.preOrderIterative(root);
+
+		System.out.println("\nThe Inorder traversal(iteratively) is :");
+		t.InOrderIterative(root);
+
+		System.out.println("\nThe LevelOrder traversal is :");
+		t.LevelOrderTraversal(root);
+
+		System.out.println("\nFinding Maximum Element in a Binary tree :");
+		System.out.println(t.MaxElementinBT(root));
+		System.out.println(t.MaxElementinBT_PreOrder(root));
+
+		System.out.println("\n--------Tree 2: ------ ");
+		Queue<Treenode> queue = new LinkedList<Treenode>();
+		;
+		Treenode<Integer> tree2 = null;
+
+		for (int i = 10; i <= 16; i++) {
+			if (tree2 == null)
+				tree2 = t.insertinBT_Recursive(null, i, queue);
+			else
+				tree2 = t.insertinBT_Recursive(tree2, i, queue);
+		}
+
+		t.LevelOrderTraversal(tree2);
+
+		System.out.println("\nFinding the size of the tree :");
+		System.out.print(t.SizeOfBT(tree2));
+
+		System.out.println("\nSearching an Element in a Binary Tree :");
+		Treenode curr = t.SearchElementinBT(tree2, 10);
+		t.preOrderTraversal(curr);
+
+		System.out.println("\nHeight of the tree:");
+		System.out.println(t.height(tree2));
+
+		System.out.println("\n--------Tree 3: ------ ");
+		Treenode<Integer> tree3 = null;
+		int height = 0;
+
+		for (int i = 10; i <= 17; i++) {
+			if (tree3 == null)
+				tree3 = t.insertinBT_Recursive(null, i, queue);
+			else
+				height = t.insertinBTRecursive_height(tree3, i, queue);
+			System.out.print(height +" ");
+		}
+        
+		System.out.println("Tree 3 using BFS: ");
+		t.LevelOrderTraversal(tree3);
+
+		System.out.println("\n\nWidth of the tree: ");
+		System.out.println(t.widthOfTree(tree3));
 
 	}
 
--- src/Trees/Treenode.java
+++ src/Trees/Treenode.java
@@ -1,15 +1,20 @@
 package Trees;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 
 public class Treenode<T> {
 
 	T data;
-	Treenode left;
-	Treenode right;
-	Treenode parent;
+	Treenode<T> left;
+	Treenode<T> right;
+	Treenode<T> parent;
+
+	int height;
 
 	Treenode(T item) {
 		data = item;
@@ -18,15 +23,19 @@ public class Treenode<T> {
 		parent = null;
 	}
 
+	Treenode() {
+
+	}
+
 	T getData() {
 		return data;
 	}
 
-	public Treenode getLeft() {
+	public Treenode<T> getLeft() {
 		return left;
 	}
 
-	public Treenode getRight() {
+	public Treenode<T> getRight() {
 		return left;
 	}
 
@@ -38,6 +47,19 @@ public class Treenode<T> {
 		this.right = right;
 	}
 
+}
+
+class Tree {
+
+	int maxdata = Integer.MIN_VALUE;
+
+	/**
+	 * Pre-Order Traversal of a Binary Tree. (Data Left Right) or (Data Right
+	 * Left)
+	 * 
+	 * @param root
+	 */
+
 	void preOrderTraversal(Treenode root) {
 		if (root == null)
 			return;
@@ -47,6 +69,13 @@ public class Treenode<T> {
 		preOrderTraversal(root.right);
 
 	}
+
+	/**
+	 * In Order Traversal of a Binary Tree. ( Left Data Right) or (Right Data
+	 * Left)
+	 * 
+	 * @param root
+	 */
 
 	public void InOrderTraversal(Treenode root) {
 		if (root == null)
@@ -58,6 +87,13 @@ public class Treenode<T> {
 
 	}
 
+	/**
+	 * Post Order Traversal of a Binary Tree. (Left Right Data) or (Right Left
+	 * Data)
+	 * 
+	 * @param root
+	 */
+
 	public void postOrderTraversal(Treenode root) {
 		if (root == null)
 			return;
@@ -68,6 +104,11 @@ public class Treenode<T> {
 
 	}
 
+	/**
+	 * * Pre Order Traversal of a Binary Tree
+	 * 
+	 * @param root
+	 */
 	public void preOrderIterative(Treenode root) {
 		Stack st = new Stack();
 		Treenode current = root;
@@ -89,6 +130,7 @@ public class Treenode<T> {
 		}
 	}
 
+	/** In Order Traversal of a Binary Tree */
 	public void InOrderIterative(Treenode root) {
 
 		Stack<Treenode> st = new Stack<Treenode>();
@@ -116,6 +158,7 @@ public class Treenode<T> {
 		}
 	}
 
+	/** Post Order Traversal of a Binary Tree */
 	public void postOrderIterative(Treenode root) {
 		Stack st = new Stack();
 		Treenode current = root;
@@ -123,6 +166,11 @@ public class Treenode<T> {
 
 	}
 
+	/**
+	 * Breadth first Search or Level Order Traversal of a Binary Tree.
+	 * 
+	 * @param root
+	 */
 	public void LevelOrderTraversal(Treenode root) {
 		Queue<Treenode> myQueue = new LinkedList<Treenode>();
 
@@ -142,21 +190,281 @@ public class Treenode<T> {
 
 	}
 
-	public Treenode InsertBT(Treenode root, T data) {
-
+	/**
+	 * Inserting node in a Binary Tree. We can add wherever we want but we will
+	 * use level order traversal and insert Element wherever we found the left
+	 * or Right child is equal to NULL. Method : We are using Queue is maintain
+	 * Breadth first Search Traversal for each node and adding it to queue and
+	 * checking whether its left and right child is available or not. *
+	 */
+	public Treenode insertInBinaryTree(Treenode root, int data) {
 		if (root == null) {
-			root = new Treenode<T>(data);
+			root = new Treenode(data);
 			return root;
 		}
+		Queue<Treenode> queue = new LinkedList<Treenode>();
 
-		if ((int) root.data >= (int) data) {
-			InsertBT(root.left, data);
-			root.left = new Treenode<T>(data);
-		} else{
-			InsertBT(root.right, data);
-			root.right = new Treenode<T>(data);
+		queue.offer(root);
+
+		while (!queue.isEmpty()) {
+			Treenode polled = queue.poll();
+			if (polled != null) {
+				if (polled.left != null)
+					queue.offer(polled.left);
+				else {
+					polled.left = new Treenode(data);
+					// polled.left.height=root.height+1;
+					return root;
+				}
+				if (polled.right != null) {
+					queue.offer(polled.right);
+				} else {
+					polled.right = new Treenode(data);
+					// polled.right.height=root.height+1;
+					return root;
+				}
+			}
 		}
 		return root;
-	
 	}
+
+	public Treenode<Integer> insertinBT_Recursive(Treenode node, int data, Queue<Treenode> q) {
+
+		if (node == null) {
+			node = new Treenode<Integer>(data);
+			return node;
+		}
+
+		Treenode<Integer> root = node;
+
+		if (root.left == null) {
+			root.left = new Treenode<Integer>(data);
+			// root.left.height=root.height+1;
+			return node;
+		} else if (root.right == null) {
+			root.right = new Treenode<Integer>(data);
+			// root.right.height=root.height+1;
+			return node;
+		} else {
+			q.add(root.left);
+			q.add(root.right);
+		}
+
+		insertinBT_Recursive(q.poll(), data, q);
+		return node;
+	}
+
+	public int insertinBTRecursive_height(Treenode node, int data, Queue<Treenode> q) {
+
+		if (node == null) {
+			node = new Treenode<Integer>(data);
+			node.height = 0;
+			return node.height;
+		}
+
+		Treenode<Integer> root = node;
+
+		if (root.left == null) {
+			root.left = new Treenode<Integer>(data);
+			root.left.height = root.height + 1;
+			return root.left.height;
+		} else if (root.right == null) {
+			root.right = new Treenode<Integer>(data);
+			root.right.height = root.height + 1;
+			return root.right.height;
+		} else {
+			q.add(root.left);
+			q.add(root.right);
+		}
+
+		Treenode polled = q.peek();
+		if (polled.left != null && polled.right != null) {
+			q.remove();
+			polled=q.peek();
+		}
+		int height = insertinBTRecursive_height(polled, data, q);
+		return height;
+
+	}
+
+	/**
+	 * Maximum Element in a Binary Tree. Search the maximum element in left
+	 * subtree and search the maximum element in right subtree.Compare them with
+	 * the element at the root and return the maximum. Complexity : O(n) using
+	 * level order Traversal i.e BFS Space Complexity :O(n)
+	 * 
+	 * @param root
+	 */
+
+	public int MaxElementinBT(Treenode root) {
+		if (root == null)
+			return 0;
+
+		Queue<Treenode> queue = new LinkedList<Treenode>();
+
+		if (maxdata < (int) root.data)
+			maxdata = (int) root.data;
+
+		if (root.left != null)
+			queue.offer(root.left);
+		if (root.right != null)
+			queue.offer(root.right);
+
+		while (!queue.isEmpty()) {
+			MaxElementinBT(queue.poll());
+		}
+
+		// System.out.println(maxdata);
+		return maxdata;
+	}
+
+	/**
+	 * Maximum Element in Binary Tree using Pre order Traversal DFS No extra
+	 * Space complexity
+	 * 
+	 * @param root
+	 */
+
+	public int MaxElementinBT_PreOrder(Treenode root) {
+		if (root == null) {
+			return maxdata;
+		}
+
+		if (maxdata < (int) root.data) {
+			maxdata = (int) root.data;
+		}
+		MaxElementinBT_PreOrder(root.left);
+		MaxElementinBT_PreOrder(root.right);
+
+		return maxdata;
+	}
+
+	/**
+	 * Search an Element in a Binary Tree.If we pass the root of a Tree, it
+	 * should return boolean value to indicate element is found or not.
+	 */
+	public Treenode SearchElementinBT(Treenode root, int data) {
+		if (root == null)
+			return null;
+
+		Treenode leftroot = null, rightroot = null;
+
+		leftroot = SearchElementinBT(root.left, data);
+		rightroot = SearchElementinBT(root.right, data);
+		if ((int) root.data == data) {
+			return root;
+		} else if (leftroot != null)
+			return leftroot;
+		else if (rightroot != null)
+			return rightroot;
+
+		return null;
+	}
+
+	/**
+	 * Size of the Binary Tree. The total number of nodes in the binary tree
+	 * (including root of the tree)
+	 * 
+	 * @param root
+	 */
+	public int SizeOfBT(Treenode node) {
+		int size = 0;
+		if (node == null)
+			return 0;
+		size++;
+		size = size + SizeOfBT(node.left);
+		size = size + SizeOfBT(node.right);
+
+		return size;
+	}
+
+	/**
+	 * Level Order Traversal in reverse Order.
+	 * Method : We are using a Stack and a Queue. 
+	 * In post order traversal(LRD) rather than printing the last element , we are adding it to
+	 * the stack using level order traversal 
+	 * 
+	 *          *10*
+	 *    20*          *30
+	 * 40*   *50     *60    *70
+	 * 
+	 * Stack : 10 20 30
+	 * Queue : 40 50 60 70
+	 * 
+	 * Finally transfer all the elements in the queue to the top of the stack .
+	 * Print all the elements in Stack in LIFO order.
+	 * @param root
+	 */
+	public void LevelOrderReverse(Treenode root) {
+		if (root == null)
+			return;
+		Queue<Treenode> q = new LinkedList<Treenode>();
+		Stack<Treenode> stack = new Stack<Treenode>();
+		
+		q.offer(root);
+		
+		while(!q.isEmpty()){
+			if(root.left!=null)
+				q.add(root.left);
+			if(root.right!=null)
+				q.add(root.right);
+			
+		stack.push	(root);
+		}
+		
+		System.out.print(stack.pop()+" ");
+
+	}
+
+	/**
+	 * Height of a tree in Binary tree.
+	 * 
+	 */
+	public int height(Treenode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		int leftheight = 1;
+		leftheight = leftheight + height(root.left);
+
+		int rightheight = 1;
+		rightheight = rightheight + height(root.left);
+
+		if (leftheight > rightheight) {
+			return leftheight;
+		} else {
+			return rightheight;
+		}
+
+	}
+
+	/** Diameter of a tree (width of the tree) */
+	List<Integer> list = new ArrayList<Integer>();
+	public int widthOfTree(Treenode root) {
+
+		List<Integer> list = diameter(root);
+
+		//Sorting the list to get two top elements in a list
+		Collections.sort(list, Collections.reverseOrder());
+
+		int diameter = list.get(0) + list.get(1)+1;
+
+		return diameter;
+
+	}
+
+	public List<Integer> diameter(Treenode root) {
+
+		if (root == null)
+			return null;
+
+		diameter(root.left);
+		diameter(root.right);
+		
+		list.add(root.height);
+		return list;
+
+	}
+
 }
